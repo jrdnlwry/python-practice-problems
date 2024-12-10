06159f86376789db6c4a349e948b55978973a9db
jrdnlowry <tatelwry@gmail.com>
2024-12-10 16:22:15 -0500

    updated code

--- goodVevil.py
+++ goodVevil.py
@@ -16,90 +16,87 @@ for each string input
 Next, sum the total up on each side to determine who wins
 """
 
-# def good_vs_evil(good, evil):
-#   g = 0
-#   e = 0
-#   good = good.split(" ")
-#   evil = evil.split(" ")
-#   for a in (good):
-#     g += int(a)
-#   for b in (evil):
-#     e += int(b)
-#   print(g)
-#   print(e)
+def good_vs_evil(good, evil):
 
-#   if g > e:
-#     return "Battle Result: Good triumphs over Evil"
-#   elif g < e:
-#     return "Battle Result: Evil eradicates all trace of Good"
-#   else:
-#     return "Battle Result: No victor on this battle field"
+  goodDict = {
+      'hobbits': {
+          'count': 0,
+          'value': 1
+      },
+      'Men': {
+          'count': 0,
+          'value': 2
+      },
+      'Elves': {
+          'count': 0,
+          'value': 3
+      },
+      'Dwarves': {
+          'count': 0,
+          'value': 3
+      },
+      'Eagles': {
+          'count': 0,
+          'value': 4
+      },
+      'Wizards': {
+          'count': 0,
+          'value': 10
+      }
+  }
 
-goodDict = {
-    'hobbits': {
-        'count': 0,
-        'value': 1
-    },
-    'Men': {
-        'count': 0,
-        'value': 2
-    },
-    'Elves': {
-        'count': 0,
-        'value': 3
-    },
-    'Dwarves': {
-        'count': 0,
-        'value': 3
-    },
-    'Eagles': {
-        'count': 0,
-        'value': 4
-    },
-    'Wizards': {
-        'count': 0,
-        'value': 10
-    }
-}
+  evilDict = {
+      'orcs': {
+          'count': 0,
+          'value': 1
+      },
+      'Men': {
+          'count': 0,
+          'value': 2
+      },
+      'wargs': {
+          'count': 0,
+          'value': 2
+      },
+      'goblines': {
+          'count': 0,
+          'value': 2
+      },
+      'uruk hai': {
+          'count': 0,
+          'value': 3
+      },
+      'trolls': {
+          'count': 0,
+          'value': 5
+      },
+      'wizards': {
+          'count': 0,
+          'value': 10
+      }
+  }
 
-evilDict = {
-    'orcs': {
-        'count': 0,
-        'value': 1
-    },
-    'Men': {
-        'count': 0,
-        'value': 2
-    },
-    'wargs': {
-        'count': 0,
-        'value': 2
-    },
-    'goblines': {
-        'count': 0,
-        'value': 2
-    },
-    'uruk hai': {
-        'count': 0,
-        'value': 3
-    },
-    'trolls': {
-        'count': 0,
-        'value': 5
-    },
-    'wizards': {
-        'count': 0,
-        'value': 10
-    }
-}
+  goodTotal = 0
+  evilTotal = 0
 
-# testing
-print(goodDict)
+  good = good.split(" ")
+  evil = evil.split(" ")
 
-good = '1 0 0 0 1 0'
-good = good.split(" ")
+  for g, num in zip(goodDict, good):
+    goodDict[g]['count'] += int(num)
 
-for g, num in zip(goodDict, good):
-  goodDict[g]['count'] += int(num)
+  for i in goodDict:
+    goodTotal += goodDict[i]["count"] * goodDict[i]["value"]
 
-print(goodDict)
+  for e, num in zip(evilDict, evil):
+    evilDict[e]['count'] += int(num)
+
+  for i in evilDict:
+    evilTotal += evilDict[i]["count"] * evilDict[i]["value"]
+   
+  if goodTotal > evilTotal:
+    return "Battle Result: Good triumphs over Evil"
+  elif goodTotal < evilTotal:
+    return "Battle Result: Evil eradicates all trace of Good"
+  else:
+    return "Battle Result: No victor on this battle field"
