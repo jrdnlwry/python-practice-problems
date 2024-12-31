0175698a383963b399b214ad2339c632afc718d3
jrdnlowry <tatelwry@gmail.com>
2024-12-31 17:31:24 -0500

    updates to maze runner algorithm

--- mazeRunner.py
+++ mazeRunner.py
@@ -14,6 +14,8 @@ def maze_runner(maze, directions):
     direction = directions
     
     # identify the starting point of the maze
+    # turn the maze into a numpy array
+    maze = np.array(maze)
     sCol = np.where(maze == 2)[0][0]
     sRow = np.where(maze == 2)[1][0]
 
@@ -26,6 +28,8 @@ def maze_runner(maze, directions):
 
     Next: iterate through direction list to determine how far we should move.
     """
+    status = ""
+
     moveDict = {
     "N":{
         'value':-1
@@ -41,18 +45,56 @@ def maze_runner(maze, directions):
         }
     }
 
+    """
+    This needs to be updated to a while loop
+    On each iteration need to check the location of where I am at within the maze.
+    If I am on a tile or section with a 3 then terminate and return Finish
+    """
     for i in direction:
         if i == "N":
             sCol = sCol + moveDict[i]['value']
             print(sCol, maze[sCol])
-            print()
+            # print()
             # print(maze[sCol-sCol+moveDict[i]['value']])
             # print(maze[sCol][sRow])
         elif i == "E":
             sRow = sRow + moveDict[i]['value']
+            print(sRow, maze[sCol][sRow])
         elif i == "W":
             sRow = sRow + moveDict[i]['value']
         elif i == "S":
             sCol = sCol + moveDict[i]['value']
 
-    pass
+    print(maze[sCol][sRow])
+    
+    print(maze[sCol])
+
+    """
+    Attempt to use a Try & Except clause
+    If the index is out of range then we assume the user dead
+    """
+    try:
+        if maze[sCol][sRow] == 1:
+            status = "Dead"
+        elif maze[sCol][sRow] == 0:
+            status = "Lost"
+        else:
+            status = "Finish"
+    except IndexError:
+        status = "Dead"
+
+    # print(status)
+    return status
+
+m = [[1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,3],
+        [1,0,1,0,1,0,1],
+        [0,0,1,0,0,0,1],
+        [1,0,1,0,1,0,1],
+        [1,0,0,0,0,0,1],
+        [1,2,1,0,1,0,1]]
+
+d = ["N","N","N","N","N","E","E","E","E","E","W","W"]
+runner_update = maze_runner(m, d)
+
+print(runner_update)
